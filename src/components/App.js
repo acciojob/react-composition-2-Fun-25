146503cfdc656d0857e0316a05872028bf763a54
Fun-25 <muskandodmani760@gmail.com>
2023-06-20 12:58:58 +0530

    submitting code-Tue Jun 20 2023 12:58:56 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [showModal, setShowModal] = useState(false);
 
-const App = () => {
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={handleShowModal}>Show Modal</button>
+      <Modal show={showModal} onClose={handleCloseModal}>
+        <p>This is the content of the modal.</p>
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
